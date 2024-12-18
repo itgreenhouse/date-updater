b60161602e30275c97af42dd2af22ab9fa84b3e9
itgreenhouse <it@greenhouse.ca>
2024-12-18 18:45:19 +0000

    Date updater added with scheduling functionality to run every 5 minutes

---
+++ dear-sales-updater/src/index.js
@@ -0,0 +1,23 @@
+// server.js
+const express = require('express');
+const { fetchSaleList, fetchSale, fetchAndUpdateSales, startScheduler } = require('./dearApi');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+app.get('/api/dear/sale', async (req, res) => {
+    try {
+        const tasks = await startScheduler();
+        // // const tasks = await fetchSaleList();
+        // const tasks = await fetchSale('e1033342-19b0-4117-893a-765b3a1cc981');
+        res.json({ success: true, data: tasks });
+    } catch (error) {
+        res.status(500).json({ success: false, error: 'Error fetching tasks from Limble' });
+    }
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
