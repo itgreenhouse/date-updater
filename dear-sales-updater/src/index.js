38015bb2a066c215fc8eb3f3b0d9aeaaff23a047
itgreenhouse <it@greenhouse.ca>
2024-12-19 01:31:05 +0000

    Added route for cron job

--- dear-sales-updater/src/index.js
+++ dear-sales-updater/src/index.js
@@ -8,6 +8,10 @@ const PORT = process.env.PORT || 3000;
 
 startScheduler();
 
+app.get('/', (req, res) => {
+    res.send('<h1>Cron job endpoint is working! Server is up!</h1>');
+});
+
 // Start the server
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
